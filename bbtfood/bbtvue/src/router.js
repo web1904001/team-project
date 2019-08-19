22c891986ae74237941a3c958d2aad76e0d6d8ec
lyh <lyh@tedu.cn>
2019-08-19 20:16:33 +0800

    111

--- bbtfood/bbtvue/src/router.js
+++ bbtfood/bbtvue/src/router.js
@@ -5,12 +5,14 @@ import Details from './views/Details'
 import NotFound from './views/NotFound'
 import Products from './views/Products'
 import MyHeader from './components/MyHeader'
+import Login from './views/Login'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {path:"/", component:MyHeader},
+    {path:"/", component:Login},
+    //{path:"/", component:MyHeader},
     // {path:"/", component:Index},
     {path:"/details/:lid", component: Details, props:true},
     {path:"/products/:kw", component:Products, props:true},
---
+++ bbtfood/bbtvue/src/views/Login.vue
@@ -0,0 +1,55 @@
+<template>
+    <div id="main" class="clearfix">
+        ::before
+        <div class="bgsi">
+            <div class="shic">
+                <img src="" alt="">
+            </div>
+        </div>
+        <div class="login clearfix">
+            ::before
+            <div class="log">
+                <h2>登录</h2>
+                <span>
+                    <a href="#">去注册</a>
+                </span>
+            </div>
+            <div class="logone">
+                <div class="iput">
+                    <input id="username" placeholder="请输入手机号/邮箱" class="dlsi r5" type="text">
+                </div>
+                <div>
+                    <input id="password" placeholder="请输入密码" class="dlsi r5" maxlength="20" type="password">
+                </div>
+                <div class="fgt">
+                    <a href="#">忘记密码</a>
+                    <span id="err" class="twro" style="margin-left: 20px;visibility:visible" >密码不能小于6位</span>
+                </div>
+                <div class="btn">
+                    <button id="login" class="btn r3" style="background: #FFB31A">登录</button>
+                </div>
+                <div class="otth">
+                    <h3>使用社交账号登录棒棒糖美食</h3>
+                    <ul>
+                        <li>
+                            <a href="#">
+                                <i class="tx"></i>
+                                "腾讯账号"
+                            </a>
+                            <a href="#" class="ml36">
+                                <li class="xlwb"></li>
+                                "新浪微博"
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+
+</script>
+<style>
+
+</style>
